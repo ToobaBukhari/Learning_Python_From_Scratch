f3f04b6c9df9ffbd7633d33bc9f930d02fb70f83
ToobaBukhari <140112749+ToobaBukhari@users.noreply.github.com>
2023-10-28 01:13:06 +0500

    initial commit

---
+++ 04-Operators_in_python/main.py
@@ -0,0 +1,29 @@
+# Arithmetic operators
+# addition
+a = 10
+b = 5
+print(a + b)
+# subtraction
+print(a - b)
+# multiplication
+print(a * b)
+# division
+print(b / a)
+# modulus
+print(b % a)
+# exponentiation
+print(a ** b)
+# floor division
+print(a // b)
+
+# Python Comparison Operators
+x = 5
+y = 10
+print(x == y ) # equal to
+# it returns false because 5 is not equal to 10
+
+print(x != y) # not equal to
+print(y > x) # greater than 
+print(x < y) # less than
+print(x >= y) # greater than equal to
+print(x <= y) # less than equal to
