7c78b86c2671b6a63a1eb97362fe5b7c2df1705a
ToobaBukhari <140112749+ToobaBukhari@users.noreply.github.com>
2023-10-27 08:21:38 +0500

    init

---
+++ 03-Variables_and_DataTypes/main.py
@@ -0,0 +1,38 @@
+x = 5      #type of x is int
+y = "Tooba"  #type of y is string
+print(x)
+print(y)
+
+x = 5
+y = "Tooba"
+print(type(x))
+print(type(y))
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(type(x))
+print(type(y))
+print(type(z))
+print(x)
+print(y)
+print(z)
+
+a,b,c,d,e,f,g = "Red","Orange","Yellow","Green","Blue","Indigo","Violet"
+print(a)
+print(f)
+print(d)
+
+a = "Python"
+
+def myfunc():
+    print("I am excited to learn " + a)
+
+myfunc()
