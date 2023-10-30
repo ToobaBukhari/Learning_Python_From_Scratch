a4fed20dc43c185f75c39674b6e0dc04f74d64ed
ToobaBukhari <140112749+ToobaBukhari@users.noreply.github.com>
2023-10-30 06:11:08 +0500

    initial commit

---
+++ 05-Exercise1-Creating an calculator/main.py
@@ -0,0 +1,20 @@
+# Exercise 01
+
+number1  = 20
+number2  = 30
+
+answer1 = number1 + number2
+answer2 = number2 - number1
+answer3 = number1 * number2
+answer4 = number1 / number2
+answer5 = number1 % number2
+
+print("Addition of",number1 ,"and",number2, "is",answer1) 
+print("Subtraction of",number1 ,"and",number2, "is",answer2) 
+print("Multiplication of",number1 ,"and",number2, "is",answer3) 
+print("Division of",number1 ,"and",number2, "is",answer4) 
+print("Modulus of",number1 ,"and",number2, "is",answer5) 
+
+
+
+
